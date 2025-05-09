bf7d955e55bdff65e9b6b19b51b3f4bf133b1a0f
罗舒雅 <1763081415@qq.com>
2025-05-09 18:58:50 +0800

    feat: 产品详情页面面包屑组件

--- static/template/header-product.js
+++ static/template/header-product.js
@@ -2,5 +2,6 @@
   $(document).ready(function(){
     $("#footer-template").load("../../static/template/footer.html");  
     $("#header-template").load("../../static/template/header-product.html");
+    $('#breadcumb-template').load("../../static/template/breadcumb-product.html");
   });
 })(jQuery);
